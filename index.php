4fbb1d1830b0a4294f72ae5faac1bceb4627c521
Alvin Lam <atklam123@gmail.com>
2021-05-11 19:24:24 -0500

    updated files

--- index.php
+++ index.php
@@ -1,24 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login</title>
+    <title>Login/Registration</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="container">
-        <img src="images/login-logo.png"/>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-input">
-                <input type="text" name="username" required="required" placeholder="Enter your username">
+    <div class="hero">
+        <div class="form-box">
+            <img src="images/login-logo.png"/>
+            <div class="button-box">
+                <div id="btn"></div>
+                <button type="button" class="toggle-btn" onclick="login()">Login</button>
+                <button type="button" class="toggle-btn" onclick="register()">Register</button>
             </div>
-            <div class="form-input">
-                <input type="password" name="password"  required="required" placeholder="Enter your password">
-            </div>
-            <input type="submit" class="btn-login" value="Login">
-            <p>Don't have an account? <a href="register.php">Register here</a></p>
-        </form>
+            <form id="login" class="input-group" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <input type="text"     class="input-field" name="username"  placeholder="Enter your username" required>
+                <input type="password" class="input-field" name="password"  placeholder="Enter your password" required>
+                <button type="submit"  class="login-btn">Login</button>
+            </form>
+            <form id="register" class="input-group" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <input  type="text"     class="input-field" name="email"    placeholder="Enter your email"    required>
+                <input  type="text"     class="input-field" name="username" placeholder="Enter your username" required>
+                <input  type="password" class="input-field" name="password" placeholder="Enter your password" required>
+                <button type="submit"   class="register-btn">Register</button>
+            </form>
+        </div>
     </div>
+
+    <script>
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        var z = document.getElementById("btn");
+
+        function login() {
+            x.style.left = "0px";
+            y.style.left = "500px";
+            z.style.left = "0";
+        }
+
+        function register() {
+            x.style.left = "-500px";
+            y.style.left = "0px";
+            z.style.left = "110px";
+        }
+    </script>
 </body>
 </html>
