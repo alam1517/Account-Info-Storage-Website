86173a9a9e75e5da8b491cc1f50839ad4a61d020
Alvin Lam <atklam123@gmail.com>
2021-05-15 13:59:09 -0500

    updated/move code

--- index.php
+++ index.php
@@ -1,3 +1,69 @@
+<?php
+// Initalize the session
+session_start();
+
+// Check for already loggin in user. If yes, redirect to user's page
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
+    header("location: user.php");
+    exit;
+}
+
+// Include config.php
+require_once "config.php";
+
+// Initialized Variables
+$username = $password = $email = "";
+$username_err = $password_err = $email_err = $login_err = "";
+
+// Process submitted form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Store username
+    $username = trim($_POST["username"]);
+
+    // Store password
+    $password = trim($_POST["password"]);
+
+    // Store email
+    $email = trim($_POST["email"]);
+
+    // Login
+    if (empty($email)) {
+        // Prepare SQL select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        if ($stmt = $mysqli->prepare($sql)) {
+            // Bind variables as parameters and set it
+            $stmt->bind_param("s", $param_username);
+            $param_username = $username;
+
+            // Execute the statement
+            if ($stmt->execute()) {
+                $stmt->store_result();
+                // Check if username exists. If yes, verify.
+                if ($stmt->num_rows() == 1) {
+                    $stmt->bind_result($id, $username, $acc_password);
+                    if ($stmt->fetch()) {
+                        // Verify password
+                        if ($acc_password == $password) {
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = (int) $id;
+
+                            header("location: user.php");
+                        }
+                        else {
+                            $login_err = "Invalid password.";
+                        }
+                    }
+                }
+                else {
+                    $login_err = "Invalid username.";
+                }
+            }
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +74,12 @@
 </head>
 <body>
     <div class="hero">
+        <div class="popup" id="popup1">
+            <p>This is a popup!</p>
+            <p>Overlay uses <b>box-shadow</b>.</p>
+            <p>(Doesn't block elements on background)</p>
+            <a href="#" onclick="hide('popup1')">Ok!</a>
+        </div>
         <div class="form-box">
             <img src="images/login-logo.png"/>
             <div class="button-box">
@@ -18,7 +90,8 @@
             <form id="login" class="input-group" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <input type="text"     class="input-field" name="username"  placeholder="Enter your username" required>
                 <input type="password" class="input-field" name="password"  placeholder="Enter your password" required>
-                <button type="submit"  class="login-btn">Login</button>
+
+                <button type="submit"  class="login-btn" onclick="document.getElementById('register').value = ''">Login</button>
             </form>
             <form id="register" class="input-group" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <input  type="text"     class="input-field" name="email"    placeholder="Enter your email"    required>
@@ -28,24 +101,7 @@
             </form>
         </div>
     </div>
-    
-    <!-- toggle buttons (Login and Register) script -->
-    <script>
-        var x = document.getElementById("login");
-        var y = document.getElementById("register");
-        var z = document.getElementById("btn");
 
-        function login() {
-            x.style.left = "0px";
-            y.style.left = "500px";
-            z.style.left = "0";
-        }
-
-        function register() {
-            x.style.left = "-500px";
-            y.style.left = "0px";
-            z.style.left = "110px";
-        }
-    </script>
+    <script src="script.js"></script>
 </body>
 </html>
